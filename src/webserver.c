d39d8bdf9a263bb53f81f2bb45191fadf287ae2b
sn <rock097abc@gmail.com>
2025-01-03 16:17:50 +0200

    station connects to wifi. Maybe needs testing on it

--- src/webserver.c
+++ src/webserver.c
@@ -19,6 +19,7 @@ static int s_retry_num = 0;
 static void event_handler(void *arg, esp_event_base_t event_base,
                           int32_t event_id, void *event_data)
 {
+    ESP_LOGI(TAG, "inside event_handler, event_base: %s event_id = %ld", event_base, event_id);
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
     {
         esp_wifi_connect();
@@ -94,8 +95,6 @@ static esp_err_t find_wifi() {
     wifi_sta_config_t wifi_points[CONFIG_ESP_WIFI_POINTS_NUMBER];
     form_wifi_points(wifi_points);
 
-    // wifi_config_t wifi_config;
-
     for (
         int index = 0, connected = 0;
         !connected && index < CONFIG_ESP_WIFI_POINTS_NUMBER;
@@ -104,31 +103,33 @@ static esp_err_t find_wifi() {
         wifi_config_t wifi_config = {
             .sta = wifi_points[index],
         };
+
         ESP_LOGI(TAG, "Trying to connect to SSID: %s, Password: %s (index: %d)",
                  wifi_points[index].ssid, wifi_points[index].password, index);
+
         ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
         ESP_ERROR_CHECK(esp_wifi_start());
+
         EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
                                                WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
                                                pdTRUE,
                                                pdFALSE,
                                                portMAX_DELAY);
-        if (bits & WIFI_CONNECTED_BIT)
-        {
+
+        if (bits & WIFI_CONNECTED_BIT) {
             ESP_LOGI(TAG, "Successfully connected to SSID: %s", wifi_points[index].ssid);
             connected = 1;
             err = ESP_OK;
         }
-        else if (bits & WIFI_FAIL_BIT)
-        {
+        else if (bits & WIFI_FAIL_BIT) {
             ESP_LOGW(TAG, "Failed to connect to SSID: %s", wifi_points[index].ssid);
-            ESP_ERROR_CHECK(esp_wifi_stop());
-        }
-        else
-        {
+        } else {
             ESP_LOGE(TAG, "Unexpected event during Wi-Fi connection.");
-            // ESP_ERROR_CHECK(esp_wifi_stop());
         }
+
+        if (!connected)
+            ESP_ERROR_CHECK(esp_wifi_stop());
 
     }
 
@@ -146,7 +147,7 @@ static esp_err_t connect_wifi(void)
 
     ESP_ERROR_CHECK(esp_netif_init());
 
-    ESP_ERROR_CHECK(esp_event_loop_create_default());   // тут в новому коді виникала помилка
+    ESP_ERROR_CHECK(esp_event_loop_create_default());   // тут в старому коді виникала помилка
     esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -168,38 +169,6 @@ static esp_err_t connect_wifi(void)
     );
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-
-    
-    /*
-    wifi_config_t wifi_config = {
-        .sta =  wifi_points[1],
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-                                           pdFALSE,
-                                           pdFALSE,
-                                           portMAX_DELAY);
-
-    if (bits & WIFI_CONNECTED_BIT)
-    {
-        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                 CONFIG_ESP_WIFI_SSID_2, CONFIG_ESP_WIFI_PASSWORD_1);
-    }
-    else if (bits & WIFI_FAIL_BIT)
-    {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                 CONFIG_ESP_WIFI_SSID_2, CONFIG_ESP_WIFI_PASSWORD_1);
-    }
-    else
-    {
-        ESP_LOGE(TAG, "UNEXPECTED EVENT");
-    }
-    */
 
     err = find_wifi();
 
